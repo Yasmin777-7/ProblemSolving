f5f98c61d447cc4aa2712f2192ef51c5843e048e
yasmin Hammuş <ruhum1699@gmail.com>
2026-08-06 19:29:29 +0300

    Add program to read and print inverted number pattern

---
+++ problem6.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message) {
+
+	int Number;
+
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number < 0);
+
+	return Number;
+}
+
+void PrintInverteNumberPattern(int Number) {
+	
+	cout << endl;
+
+	for (int i = Number; i >= 1; i--) 
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << i;
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	PrintInverteNumberPattern(ReadPositiveNumber("Enter a positive Number ?"));
+
+	return 0;
+
+}
